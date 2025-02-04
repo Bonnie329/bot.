24302816bdddfa55376558372198afeea14a7ee9
Bonnie329 <dalorsokenneth@gmail.com>
2025-02-04 15:39:39 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,5 +23,22 @@ async def repeat(ctx, times: int, content='kenneth'):
     """Repeats a message multiple times."""
     for i in range(times):
         await ctx.send(content)
+        
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
 
 bot.run("")
